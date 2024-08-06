e133ba99cb76357efea4ee34a15a92e3243d6125
Mateusz Paulus <paulus.m.mateusz@gmail.com>
2024-08-06 19:40:34 +0200

    Feature: Create adapter and mapper for reviews

--- src/modules/reviews/adapters/rating-captain.adapter.ts
+++ src/modules/reviews/adapters/rating-captain.adapter.ts
@@ -1,25 +1,67 @@
 import { Injectable } from '@nestjs/common';
-import { PaginatedRequest, PaginatedType } from '../domain/reviews.types';
+import {
+  PaginatedRequest,
+  PaginatedType,
+  ReviewExternalRecord,
+} from '../domain/reviews.types';
+
 import { RatingCaptainMapper } from './rating-captain.mapper';
 import { RatingCaptainPort } from './rating-captain.port';
 import { ReviewEntity } from '../domain/reviews.entity';
+import * as fs from 'fs';
+import * as path from 'node:path';
 
 @Injectable()
 export class RatingCaptainAdapter implements RatingCaptainPort {
+  constructor(private readonly mapper: RatingCaptainMapper) {}
 
-  constructor(
-    private readonly mapper: RatingCaptainMapper
-  ) {}
+  async getReviews(
+    paginated: PaginatedRequest
+  ): Promise<PaginatedType<ReviewEntity>> {
+    // const { page, per_page } = paginated;
+    // const data = []; // TODO: implement this, simulate paginated response from external API
+    // const reviews = []; //TODO: implement this, map data to domain, use mapper
+    // return reviews;
 
-  async getReviews(paginated: PaginatedRequest):Promise<PaginatedType<ReviewEntity>> {
     const { page, per_page } = paginated;
-    const data = [] // TODO: implement this, simulate paginated response from external API
-    const reviews = [] //TODO: implement this, map data to domain, use mapper
-    return reviews;
+
+    const filePath = path.join(
+      process.cwd(),
+      `src/modules/reviews/adapters/external_reviews_data_example.json`
+    );
+
+    const fileData = fs.readFileSync(filePath, 'utf-8');
+    const allExternalReviews: ReviewExternalRecord[] = JSON.parse(fileData);
+
+    const total = allExternalReviews.length;
+
+    const startIndex = (page - 1) * per_page;
+    const endIndex = startIndex + per_page;
+    const paginatedReviews = allExternalReviews.slice(startIndex, endIndex);
+
+    const reviews = this.mapper.reviewsToDomain({
+      data: paginatedReviews,
+    });
+    return {
+      data: reviews.data,
+      total,
+      page,
+      per_page,
+    };
   }
 
-  async count():Promise<number> {
-    const response = 111 // TODO: implement this
-    return response;
+  async count(): Promise<number> {
+    // const response = 111; // TODO: implement this
+    // return response;
+
+    const filePath = path.join(
+      process.cwd(),
+      `src/modules/reviews/adapters/external_reviews_data_example.json`
+    );
+    const fileData = fs.readFileSync(filePath, 'utf-8');
+
+    const allExternalReviews: ReviewExternalRecord[] = JSON.parse(fileData);
+
+    return allExternalReviews.length;
   }
 }
--- src/modules/reviews/adapters/rating-captain.mapper.ts
+++ src/modules/reviews/adapters/rating-captain.mapper.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@nestjs/common';
-import { ExternalPaginatedType, ReviewExternalRecord, PaginatedType } from '../domain/reviews.types';
+import {
+  ExternalPaginatedType,
+  PaginatedType,
+  ReviewExternalRecord,
+} from '../domain/reviews.types';
 import { ReviewEntity } from '../domain/reviews.entity';
 
 @Injectable()
 export class RatingCaptainMapper {
-
-  constructor(){}
-
-  reviewsToDomain(reviews: ExternalPaginatedType<ReviewExternalRecord>): PaginatedType<ReviewEntity> {
+  reviewsToDomain(
+    reviews: ExternalPaginatedType<ReviewExternalRecord>
+  ): PaginatedType<ReviewEntity> {
     //TODO: implement this, map external reviews to domain
+    const { data } = reviews;
+    const domainReviews: ReviewEntity[] = data.map((externalReview) => {
+      return new ReviewEntity({
+        id: externalReview.id,
+        email: externalReview.email,
+        name: externalReview.name,
+        description: externalReview.description,
+        rating: externalReview.rate,
+        rate_date: new Date(externalReview.rate_date),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    });
+
+    return {
+      data: domainReviews,
+    };
+    // return [];
+  }
 }
